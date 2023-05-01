e6ee85bfaca3757a6e4acb51b8e94bbcb725887a
ShailMurtaza <shailmurtaza@gmail.com>
2023-05-01 19:58:26 +0500

    Created New Branch API which will use gogo anime API to fetch all episode links instead of loading whole site to render in browser Added API.js

---
+++ API.js
@@ -0,0 +1,34 @@
+function loadListEpisode(obj, ep_start, ep_end, id, default_ep, alias) {
+    $("#load_ep").html('<div class="loader"></div>');
+    disabledHome(obj);
+    $.ajax({
+        url:
+            base_url_cdn_api +
+            "ajax/load-list-episode?ep_start=" +
+            ep_start +
+            "&ep_end=" +
+            ep_end +
+            "&id=" +
+            id +
+            "&default_ep=" +
+            default_ep +
+            "&alias=" +
+            alias,
+        type: "GET",
+        dataType: "html",
+        async: false,
+        crossDomain: !0,
+        success: function (data) {
+            $("#load_ep").html(data);
+        },
+    });
+}
+
+if (document.getElementById('episode_page')) {
+    var ep_start = $('#episode_page a.active').attr('ep_start');
+    var ep_end = $('#episode_page a.active').attr('ep_end');
+    var id = $("input#movie_id").val();
+    var default_ep = $("input#default_ep").val();
+    var alias = $("input#alias_anime").val();
+    loadListEpisode('#episode_page a.active', ep_start, ep_end, id, default_ep, alias);
+}
