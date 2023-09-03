82856b169c48aeee601dddaeab60814e20bc25d0
ShailMurtaza <shailmurtaza@gmail.com>
2023-09-03 15:58:23 +0500

    scrap: done fetching data from url and saving it into variable

--- webApp/static/scrap.js
+++ webApp/static/scrap.js
@@ -1,27 +1,41 @@
 const url_inp = document.getElementById("url_inp")
-const output_div = document.getElementById("output")
-
+const output_cont = document.getElementById("output")
+const scrap_btn = document.getElementById("scrap_btn")
 const api_url = "https://ajax.gogo-load.com/ajax/load-list-episode" // API of GoGo Anime from where we can fetch ul list of all animes
 
 
-function scrap() {
+async function scrap() {
     let anime_url = url_inp.value
     if (anime_url) {
         anime_url = btoa(anime_url)
-        fetch_data("/fetch/" + anime_url)
+        output("FETCHING ...")
+        let html = await fetch_data("/fetch/" + anime_url)
+        console.log(html)
     }
-    else console.error("Enter URL")
+    else {
+        console.error("Enter URL")
+    }
 }
 
 
 function get_req_url() {
 }
 
-function fetch_data(url) {
-    console.log("URL: " + url)
-    fetch(url).then(resp=> {
-        return resp.text()
-    }).then(data=> {
-        console.log("FETCH: " + data)
-    })
+function output(text) {
+    output_cont.innerHTML += text = "\n"
+}
+
+async function fetch_data(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.text();
+        return data;
+    } catch (error) {
+        console.error("Error:", error);
+        throw error; // Rethrow the error for the caller to handle
+    }
 }
