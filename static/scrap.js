5e4b63b22a83292a15dde5c1ab4eeeab395c6de3
ShailMurtaza <shailmurtaza@gmail.com>
2023-09-04 12:10:02 +0500

    save_anime: Done saving anime download list in database

--- static/scrap.js
+++ static/scrap.js
@@ -6,6 +6,7 @@ const parser = new DOMParser();
 
 
 async function scrap() {
+    output("") // Clear Output
     let anime_url = url_inp.value.trim() // get url from input box
     if (!anime_url) { // if url_inp is empty
         output("Enter URL")
@@ -27,13 +28,21 @@ async function scrap() {
     output("Fetching Download List ...")
     let ep_list = await get_download_list(url_list)
     output("Done Fetching Download List ...")
-    console.log(ep_list)
+    output("Saving data in database ...")
+    let result = await save_anime(alias, ep_list)
+    if (result == "True") {
+        output("<b>Data Saved Successfully ...</b>")
+    }
+    else {
+        output("<i><b>Something Went Wrong ...</b></i>")
+    }
+    scrap_btn.disabled = false // enable scrap button
 }
 
 
 async function get_download_list(url_list) {
     let ep_list = []
-    for(let i=0;i<url_list.length/8;i++) { // Divide length for fast result. Since it is testing phase
+    for(let i=0;i<2;i++) { // Divide length for fast result. Since it is testing phase
         let html = await fetch_data("/fetch/" + btoa(url_list[i]))
         let [title, url] = get_download_data(html)
         ep_list.push([title, url])
@@ -88,20 +97,42 @@ function gen_url(data) {
 
 async function fetch_data(url) {
     try {
-        const response = await fetch(url);
+        const response = await fetch(url)
         if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
+            throw new Error(`HTTP error! Status: ${response.status}`)
         }
 
-        const data = await response.text();
-        return data;
+        const data = await response.text()
+        return data
     } catch (error) {
-        console.error("Error:", error);
-        throw error; // Rethrow the error for the caller to handle
+        console.error("ERROR:", error)
+        throw error // Rethrow the error for the caller to handle
+    }
+}
+
+
+async function save_anime(title, ep_list) {
+    try {
+        const response = await fetch("/save_anime", {
+            "method": "POST",
+            "headers": {"Content-Type": "application/json"},
+            "body": JSON.stringify({"title": title, "ep_list": ep_list})
+        })
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response}`)
+        }
+        const data = await response.text()
+        return data
+    } catch(error) {
+        console.error("ERROR:", error)
     }
 }
 
 
 function output(text) {
-    output_cont.innerHTML += text + "</br>"
+    if (text)
+        output_cont.innerHTML += text + "</br>"
+    else
+        output_cont.innerHTML = ""
 }
