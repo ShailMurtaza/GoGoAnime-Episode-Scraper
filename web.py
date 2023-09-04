6f32c0934b5d1d0ea3cbf24dcf3e2a7127c160f8
ShailMurtaza <shailmurtaza@gmail.com>
2023-09-04 12:10:02 +0500

    save_anime: Done saving anime download list in database

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-from flask import Flask, render_template, abort
+from flask import Flask, request, render_template, abort
+from flask_sqlalchemy import SQLAlchemy
 from requests import get
 from base64 import b64decode
 import logging
@@ -9,15 +10,26 @@ log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+app.app_context().push()
 
-def get_links():
-    data = open("download_links.txt").read()
-    data = data.split("\n")[:-1]
-    matrix = []
-    for i in data:
-        row = i.split(", ")
-        matrix.append(row)
-    return matrix
+
+class Anime(db.Model):
+    __tablename__ = "Anime"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    path = db.Column(db.Text, nullable=True)
+    index = db.Column(db.Integer, default=0)
+
+class EP_list(db.Model):
+    __tablename__ = "EP_list"
+    id = db.Column(db.Integer, primary_key=True)
+    anime_id = db.Column(db.Integer, nullable=False)
+    title = db.Column(db.Text, nullable=False)
+    url = db.Column(db.Text, nullable=False)
 
 
 @app.route("/")
@@ -28,6 +40,24 @@ def index():
 @app.route("/scrap")
 def scrap():
     return render_template("scrap.html")
+
+
+@app.route("/save_anime", methods=["POST"])
+def save_anime():
+    anime_dict = request.get_json()
+    anime = Anime(title=anime_dict.get("title"))
+    db.session.add(anime)
+    db.session.commit()
+
+    ep_list = anime_dict.get("ep_list")
+    for i in ep_list:
+        ep_title = i[0]
+        ep_url = i[1]
+        episode = EP_list(anime_id=anime.id, title=ep_title, url=ep_url)
+        db.session.add(episode)
+    db.session.commit()
+    
+    return "True"
 
 
 @app.route("/fetch/<url>")
@@ -47,11 +77,7 @@ def fetch_main_url():
 def fetch_list():
     return open("test_list.html", "r").read()
 
-@app.route("/get")
-def get_links():
-    matrix = get_links()
-    return str(matrix).replace("'", '"')
 
-
-app.run(host="0.0.0.0", port=8080, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
 
