26ca77c4e5886d788c60252104c688b4c2bbe3c8
ShailMurtaza <shailmurtaza@gmail.com>
2023-09-05 13:13:31 +0500

    anime: Done showing links in buttons div

--- to_dict.py
+++ to_dict.py
@@ -3,3 +3,10 @@ def anime_to_dict(anime):
     for i in anime:
         anime_dict[i.id] = i.title
     return anime_dict
+
+
+def ep_to_dict(ep):
+    ep_list = []
+    for i in ep:
+        ep_list.append([i.title, i.url])
+    return ep_list
