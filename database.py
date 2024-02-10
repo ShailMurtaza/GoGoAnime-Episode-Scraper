a2d9b5333e517c956be70c70f9012135fd63840d
ShailMurtaza <shailmurtaza@gmail.com>
2024-02-10 12:24:51 +0500

    Added comments and change table/class name form EP_list to Episode

--- database.py
+++ database.py
@@ -6,12 +6,15 @@ class Database():
         self.connection = sqlite3.connect(database, check_same_thread=False)
         self.cursor = self.connection.cursor()
 
+    # Execute query and then return result
     def query(self, q):
         return self.cursor.execute(q)
 
+    # Commit changes
     def commit(self):
         self.connection.commit()
 
+    # Fetch all anime
     def anime_all(self):
         data = self.query("SELECT * FROM Anime").fetchall()
         anime_list = []
@@ -19,39 +22,46 @@ class Database():
             anime_list.append(Anime(i))
         return anime_list
     
+    # Return specific anime using Primary Key
     def get_anime(self, ID):
         anime = self.query(f"SELECT * FROM Anime WHERE id={ID}").fetchone()
         if not anime:
             return None
         return Anime(anime)
 
+    # Take Anime object as parameter and set/update every feild of that anime using Primary Key
     def update_anime(self, anime):
         self.query(f"UPDATE Anime SET 'title'='{anime.title}', 'anime_url'='{anime.anime_url}', 'path'='{anime.path}', 'index'='{anime.index}' WHERE id='{anime.id}'")
         self.commit()
 
+    # Insert new Anime, fetch its primary key and return Anime object with its primary key
     def insert_anime(self, anime):
         result = self.query(f"INSERT INTO Anime ('title', 'anime_url', 'path', 'index') VALUES ('{anime.title}', '{anime.anime_url}', '{anime.path}', '{anime.index}') RETURNING id;")
         anime.id = result.fetchone()[0]
         self.commit()
         return anime
     
+    # Delete anime and its episodes using anime primary key
     def delete_anime(self, ID):
         self.query(f"DELETE FROM Anime WHERE id='{ID}'")
-        self.query(f"DELETE from EP_list WHERE anime_id='{ID}'")
+        self.query(f"DELETE from Episode WHERE anime_id='{ID}'")
         self.commit()
     
+    # Return list of Episode object of that anime using primary key of anime
     def anime_ep_list(self, ID):
-        data = self.query(f"SELECT * FROM EP_list WHERE anime_id={ID}").fetchall()
+        data = self.query(f"SELECT * FROM Episode WHERE anime_id={ID}").fetchall()
         ep_list = []
         for i in data:
-            ep_list.append(EP_list(i))
+            ep_list.append(Episode(i))
         return ep_list
 
+    # Take Episode object as parameter and insert new episode
     def insert_episode(self, episode):
-        self.query(f"INSERT INTO EP_list ('anime_id', 'title', 'url') VALUES ('{episode.anime_id}', '{episode.title}', '{episode.url}');")
+        self.query(f"INSERT INTO Episode ('anime_id', 'title', 'url') VALUES ('{episode.anime_id}', '{episode.title}', '{episode.url}');")
 
-    def ep_list_count(self, ID):
-        data = self.query(f"SELECT COUNT(*) FROM EP_list WHERE anime_id={ID}").fetchone()
+    # Return number of episodes of anime using its primary key
+    def episodes_count(self, ID):
+        data = self.query(f"SELECT COUNT(*) FROM Episode WHERE anime_id={ID}").fetchone()
         return data[0]
     
 
@@ -65,7 +75,7 @@ class Anime():
         self.index = data[4]
 
 
-class EP_list():
+class Episode():
     def __init__(self, data):
         self.id = data[0]
         self.anime_id = data[1]
--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from flask import Flask, request, render_template, abort, redirect
-from database import Database, Anime, EP_list
+from database import Database, Anime, Episode
 import requests
 from base64 import b64decode
 import logging
@@ -17,11 +17,12 @@ database = Database(app.config.get("database"))
 
 @app.route("/")
 def index():
-    anime = database.anime_all()
+    anime = database.anime_all() # List of all anime
     anime = anime_to_dict(anime)
     return render_template("anime.html", anime=anime)
 
 
+# get specific anime with its episode list
 @app.route("/get_anime/<int:ID>")
 def get_anime(ID):
     anime = database.get_anime(ID)
@@ -32,6 +33,7 @@ def get_anime(ID):
     return redirect("/")
 
 
+# get index of specific anime using primary key
 @app.route("/get_index/<int:ID>")
 def get_anime_index(ID):
     anime = database.get_anime(ID)
@@ -40,11 +42,13 @@ def get_anime_index(ID):
     return "False"
 
 
+# update/set index of anime
 @app.route("/set_index/<int:ID>/<int:index>")
 def set_anime_index(ID, index):
     anime = database.get_anime(ID)
     if anime:
-        ep_num = database.ep_list_count(ID)
+        ep_num = database.episodes_count(ID)
+        # Number of episodes should be greater than index being set because index starts with 0
         if ep_num >= 0 and ep_num > index:
             anime.index = index
             database.update_anime(anime)
@@ -65,15 +69,17 @@ def update_anime(ID):
     return render_template("scrap.html", anime_id=anime.id, anime_url=anime.anime_url, update=True)
 
 
+# Return number of episodes of anime
 @app.route("/anime_count/<int:ID>")
 def anime_count(ID):
     anime = database.get_anime(ID)
     if not anime:
         return "False"
-    count = database.ep_list_count(ID)
+    count = database.episodes_count(ID)
     return str(count)
 
 
+# save new anime, or update list of episode if anime already exist
 @app.route("/save_anime", methods=["POST"])
 def save_anime():
     anime_dict = request.get_json()
@@ -93,12 +99,13 @@ def save_anime():
     for i in ep_list:
         ep_title = i[0]
         ep_url = i[1]
-        episode = EP_list((None, anime.id, ep_title, ep_url))
+        episode = Episode((None, anime.id, ep_title, ep_url))
         database.insert_episode(episode)
     database.commit()
     return "True"
 
 
+# Delete anime with its episodes
 @app.route("/del_anime/<int:ID>")
 def del_anime(ID):
     anime = database.get_anime(ID)
@@ -107,6 +114,7 @@ def del_anime(ID):
     return redirect("/")
 
 
+# Update title of anime
 @app.route("/edit_title/<int:ID>", methods=["POST"])
 def edit_title(ID):
     title = request.get_json().get("title")
